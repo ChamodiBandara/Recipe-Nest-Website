7cb9fc8e2e302a13964508d63d9316a4f43a4acc
ChamodiBandara <chamodidevini@gmail.com>
2025-01-18 00:13:20 +0530

    updated version of the recipe categories pages

--- frontend/src/Recipecom/Lunch.js
+++ frontend/src/Recipecom/Lunch.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import Header from '../components/header.js';
 import Footer from '../components/footer.js';
 import '../Recipecom/Recipe.css'; 
-import cs from '../images/categories/Cajun sausage and rice.jpg';
+import MeatLoaf from '../images/categories/meatloaf.jpg';
 import FriedRice from '../images/categories/chicken fried rice.jpg';
-import Hawallian from '../images/categories/Hawaiian chicken salad.jpg';
+import Hawaiian from '../images/categories/Hawaiian chicken salad.jpg';
 import MexicanRice from '../images/categories/Mexican Rice.jpg';
-import sphagetti from '../images/categories/sphagetti.jpg';
+import Pizza from '../images/categories/Chicken pizza.jpg';
 import Noodles from '../images/categories/Chicken Teriyaki Noodles.jpg';
 
-
-
 function Lunch() {
-  // Array of dinner data)
+  const navigate = useNavigate();
+ 
   const lunchRecipes = [
-    { image: cs, title: 'Cajun Sausage and Rice',},
-    { image: FriedRice, title:'Chicken Fried Rice', },
-    { image: Hawallian, title: 'Hawaiian Chicken Salad ',},
-    { image: MexicanRice, title:'Mexican Rice', },
-    { image: Noodles, title: 'Chicken Teriyaki Noodles ',},
-    { image: sphagetti, title:'Sphagetti and Meat Balls', }
+    { image: MeatLoaf, title: 'MeatLoaf', path: '/MeatLoaf' },
+    { image: FriedRice, title: 'Chicken Fried Rice', path: '/chicken-fried-rice' },
+    { image: Hawaiian, title: 'Hawaiian Chicken Salad', path: '/hawaiian-chicken-salad' },
+    { image: MexicanRice, title: 'Mexican Rice', path: '/mexican-rice' },
+    { image: Noodles, title: 'Chicken Teriyaki Noodles', path: '/chicken-teriyaki-noodles' },
+    { image: Pizza, title: 'Chicken Pizza', path: '/chicken-pizza' }
   ];
 
   return (
@@ -28,11 +28,16 @@ function Lunch() {
         <section>
       <h1>Lunch Recipes</h1>
       <div className="recipe-list">
-        {lunchRecipes.map((recipe, index) => (
-          <div className="recipe-card" key={index}>
-            <img src={recipe.image} alt={recipe.title} />
-            <h3>{recipe.title}</h3>
-          </div>
+          {lunchRecipes.map((recipe, index) => (
+            <div
+              className="recipe-card"
+              key={index}
+              onClick={() => navigate(recipe.path)}
+            
+            >
+              <img src={recipe.image} alt={recipe.title} />
+              <h3>{recipe.title}</h3>
+            </div>
         ))}
       </div>
       </section>
